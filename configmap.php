bf63ecdd23fba8965fdcec6d7c82ad70d8eed774
Miklex <alexiglesias33@hotmail.com>
2020-06-23 19:32:51 -0500

    OCP Changes

--- configmap.php
+++ configmap.php
@@ -1,21 +1,24 @@
 <?php
 
-$jsonConfigMap = file_get_contents(getenv("CONFIG_MAP_PATH"));
-$jsonIterator = new RecursiveIteratorIterator(
-    new RecursiveArrayIterator(json_decode($jsonConfigMap, TRUE)),
-    RecursiveIteratorIterator::SELF_FIRST);
-if($jsonConfigMap) {
-foreach ($jsonIterator as $key => $val) {
-
-  echo "Configuración Cargada de " - getenv("CONFIG_MAP_PATH");
-
-    if(is_array($val)) {
-        echo "$key:\n";
-    } else {
-        echo "$key => $val\n";
-    }
+try {
+   if(getenv("CONFIG_MAP_PATH")) {
+     $jsonConfigMap = file_get_contents(getenv("CONFIG_MAP_PATH"));
+     $jsonIterator = new RecursiveIteratorIterator(
+        new RecursiveArrayIterator(json_decode($jsonConfigMap, TRUE)),
+        RecursiveIteratorIterator::SELF_FIRST);
+     foreach ($jsonIterator as $key => $val) {
+        echo "Configuración Cargada de " - getenv("CONFIG_MAP_PATH");
+           if(is_array($val)) {
+              echo "$key:\n";
+           } else {
+              echo "$key => $val\n";
+           }
+      }
+   } else {
+       echo "No hay Configuración Cargada";
+   }
+} catch(InvalidArgumentException $iae) {
+    echo "No hay Configuración Cargada";
 }
-} else {
-  echo "No hay Configuración Cargada";
-}
+
 ?>

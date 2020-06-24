f8ca5a257498c2ea213c9350f7fa631b0c6e9cb9
Miklex <alexiglesias33@hotmail.com>
2020-06-23 19:23:31 -0500

    OCP Changes

--- dbtest.php
+++ dbtest.php
@@ -9,8 +9,10 @@ try {
   $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpwd);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Estas conectado a la base de datos " .  getenv("MYSQL_DATABASE");
-  $conn->close();
+  echo "<br><br><a style='color: red' href='index.php'><- Regresar</a> ";
 } catch(PDOException $e) {
   echo "No se pudo conectar a la base de datos:: " . $e->getMessage();
 }
+
+$conn = null;
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 echo "Bienvenido a PHP MySQL Example desplegado en OpenShift Container Platform";
-echo "<br>Para probar la conexión a la base de datos ve a la siguiente liga <a style='color: red' href='dbtest.php'>Probar BD</a> ";
+echo "<br><br>Para probar la conexión a la base de datos ve a la siguiente liga <a style='color: red' href='dbtest.php'>Probar BD</a> ";
+echo "<br><br>";
+echo "<br><br>Para probar los config maps ve a la siguiente liga <a style='color: blue' href='configmap.php'>Probar Config Maps</a> ";
 
 ?>
